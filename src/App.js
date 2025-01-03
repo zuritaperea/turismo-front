4104be4563f35489f951507e2423b027a295e728
Franco Zurita Perea <francozurita@gmail.com>
2025-01-03 20:24:24 -0300

    color principal

--- src/App.js
+++ src/App.js
@@ -49,10 +49,10 @@ function App() {
       console.log('config: ', config)
       // Si la configuración se obtiene correctamente, actualizamos las variables CSS
       if (config) {
-        document.documentElement.style.setProperty('--color-principal',`${config.main_link_color} !important`);
-        document.documentElement.style.setProperty('--color-principal-background', `${config.body_background} !important`);
+        document.documentElement.style.setProperty('--color-principal', config.main_link_color);
+        document.documentElement.style.setProperty('--color-principal-background', config.body_background);
         document.title = config.title
-      }
+      } 
     };
 
 
