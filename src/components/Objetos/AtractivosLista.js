dcb1b20aaa6d1236a9e46107a917f63844e6b07d
Franco Zurita Perea <francozurita@gmail.com>
2025-03-05 10:29:22 -0300

    se borro import innecesario en atractivo lista

--- src/components/Objetos/AtractivosLista.js
+++ src/components/Objetos/AtractivosLista.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import LandingHeader from "../Landing/LandingHeader/LandingHeader";
 import LandingFooter from "../Landing/LandingFooter/LandingFooter";
