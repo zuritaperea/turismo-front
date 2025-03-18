cce0c3ca21934726ee6862903366b709aaa2b98d
Franco Zurita Perea <francozurita@gmail.com>
2025-03-18 20:19:19 -0300

    se modifico el menu del footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import logo from '../assets/img/logomark.png';
 import { ConfigContext } from '../extras/ConfigContext'; // Importa el contexto
 import SocialLinks from "./SocialLinks";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const [footerLogo, setFooterLogo] = useState(logo);
@@ -13,7 +14,7 @@ export default function Footer() {
       setFooterLogo(config.footer_logo || logo);
       setFooterDescription(
         config.footer_description ||
-          "Este es un párrafo descriptivo marketinero de cada uno de los destinos."
+        "Este es un párrafo descriptivo marketinero de cada uno de los destinos."
       );
     }
   }, [config]);
@@ -22,35 +23,46 @@ export default function Footer() {
     <footer className="hidden md:block">
       {/* Sección superior del footer */}
       <div id="menu-footer" className="bg-white mt-10 py-5 px-10 mx-auto">
-      <p className="descripcion text-center sm:mx-10 my-7 text-sm text-gray-600">
-  {footerDescription}
-</p>
-        
+        <p className="descripcion text-center sm:mx-10 my-7 text-sm text-gray-600">
+          {footerDescription}
+        </p>
+
         <div className="flex flex-col md:flex-row items-center justify-center gap-8">
-          
+
           <div className="flex justify-center">
             <img className="logo" src={footerLogo} alt="Logo Footer" />
           </div>
           <div className="flex flex-wrap justify-center gap-8">
             <div>
-              <h3 className="titulo mb-4 text-center">About us</h3>
+              <h3 className="titulo mb-4 text-center">          <Link to="/" className="font-medium text-base">Inicio</Link>
+              </h3>
             </div>
             <div>
-              <h3 className="titulo mb-4 text-center">Features</h3>
+              <h3 className="titulo mb-4 text-center">          <Link to="/alojamientos" className="font-light text-base">Alojamiento</Link>
+              </h3>
             </div>
             <div>
-              <h3 className="titulo mb-4 text-center">FAQs</h3>
+              <h3 className="titulo mb-4 text-center">          <Link to="/atractivos" className="font-light text-base">Actividades</Link>
+              </h3>
             </div>
             <div>
-              <h3 className="titulo mb-4 text-center">Blog</h3>
+              <h3 className="titulo mb-4 text-center">          <Link to="/eventos" className="font-light text-base">Eventos</Link>
+              </h3>
             </div>
             <div>
-              <h3 className="titulo mb-4 text-center">Políticas de privacidad</h3>
+              <h3 className="titulo mb-4 text-center">          <a
+                href="https://fidibonito.ciceroneweb.com/register-user"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-light text-base"
+              >
+                Planificador Inteligente
+              </a></h3>
             </div>
           </div>
         </div>
 
-       
+
 
       </div>
 
