867b6f069516535c3183aa75003b5f43b36cb7b3
NiAlvarez <alvareznicolasdev@gmail.com>
2025-04-29 21:27:56 -0300

    fix descripcion alojamiento

--- src/components/DescripcionBilingue.jsx
+++ src/components/DescripcionBilingue.jsx
@@ -1,4 +1,6 @@
 export const SeccionDescripcionMultilingue = ({ titulo, descripcion }) => {
+  if (!descripcion) return null;
+
   const bloques = descripcion.split(/---+/).map(b => b.trim());
 
   const contenidoPOR = bloques.find(b =>
@@ -11,6 +13,8 @@ export const SeccionDescripcionMultilingue = ({ titulo, descripcion }) => {
   const cleanText = (bloque) =>
     bloque.replace(/^POR|^ESP|^🇧🇷|^🇪🇸/, '').trim();
 
+  const noContenidoMultilingue = !contenidoESP && !contenidoPOR;
+
   return (
     <section className="w-full mt-[60px] sm:mt-[80px] md:mt-[120px]">
       <h2 className="text-3xl font-semibold text-center text-[#101828] dark:text-white mb-10">
@@ -20,19 +24,26 @@ export const SeccionDescripcionMultilingue = ({ titulo, descripcion }) => {
       <div className="w-full space-y-12">
         {contenidoPOR && (
           <div>
-            <p className="text-sm sm:text-base italic font-bold  mb-3">🇧🇷 POR</p>
+            <p className="text-sm sm:text-base italic font-bold mb-3">🇧🇷 POR</p>
             <p className="whitespace-pre-line text-[17px] text-slate-800 leading-relaxed">
               {cleanText(contenidoPOR)}
             </p>
           </div>
         )}
+
         {contenidoESP && (
           <div>
-            <p className="text-sm sm:text-base italic font-bold  mb-3">🇪🇸 ESP</p>
+            <p className="text-sm sm:text-base italic font-bold mb-3">🇪🇸 ESP</p>
             <p className="whitespace-pre-line text-[17px] text-slate-800 leading-relaxed">
               {cleanText(contenidoESP)}
             </p>
           </div>
+        )}
+
+        {noContenidoMultilingue && (
+          <p className="whitespace-pre-line text-[17px] text-slate-800 leading-relaxed">
+            {descripcion}
+          </p>
         )}
       </div>
     </section>
