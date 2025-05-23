313190a20c8c1ba1098ab594bce1d4de5e24043a
Franco Zurita Perea <francozurita@gmail.com>
2025-05-23 20:23:40 -0300

    se agregó para modificar en el backend los items de acceso directo y se modifico en el front

--- src/components/DirectAccessList.jsx
+++ src/components/DirectAccessList.jsx
@@ -1,32 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import pasaporte from '../assets/img/pasaporte-turistico.png';
-import planifica from '../assets/img/planifica-viaje.png';
-import gestion from '../assets/img/gestion-reservas.png';
 
-const items = [
-  {
-    title: 'Pasaporte turístico',
-    description: 'Explorá nuestro destino y obtené beneficios exclusivos.<br />¡Viajá, disfrutá y ganá!',
-    image: pasaporte,
-    url: '/pasaporte',
-  },
-  {
-    title: 'Gestión de reservas',
-    description: 'Consultá, reservá y organizá tus actividades turísticas de forma fácil y rápida, todo en un solo lugar.',
-    image: gestion,
-    url: '/marketplace',
-  },
-  // {
-  //   title: 'Planifica tu viaje',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet sapien eget diam commodo semper.',
-  //   image: planifica,
-  //   url: '/planifica',
-  // },
-];
+const DirectAccessList = ({ items }) => {
 
-const DirectAccessList = () => {
-  const isCentered = items.length === 2;
+  const filteredItems = items.filter(item => item.published === true);
+  const isCentered = filteredItems.length === 2;
 
   return (
     <div className="flex justify-center px-4">
@@ -37,29 +15,50 @@ const DirectAccessList = () => {
           max-w-screen-lg
         `}
       >
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center text-center p-6 "
-          >
-            <img src={item.image} alt={item.title} className="w-20 h-20 mb-4" />
-            <h3 className="text-xl font-semibold text-gray-800 mb-2">{item.title}</h3>
-            <p
-              className="text-gray-600 text-base mb-4"
-              dangerouslySetInnerHTML={{ __html: item.description }}
-            />
-            <Link
-              to={item.url}
-              className="bg-gray-600 text-white text-sm px-6 py-2 rounded-xl hover:bg-gray-800 transition"
+        {filteredItems.map((item, index) => {
+          console.log(item);
+          const imageUrl = item.image_url
+            ? (item.image_url.startsWith('http') ? item.image_url : `${process.env.REACT_APP_API_URL}${item.image_url}`)
+            : item.image;
+
+          const isExternal = item.url.startsWith('http://') || item.url.startsWith('https://');
+
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center text-center p-6"
             >
-              Ver &rsaquo;
-            </Link>
-          </div>
-        ))}
+              <img src={imageUrl} alt={item.title_text} className="w-20 h-20 mb-4" />
+              <h3 className="text-xl font-semibold text-gray-800 mb-2">{item.title_text}</h3>
+              <p
+                className="text-gray-600 text-base mb-4"
+                dangerouslySetInnerHTML={{ __html: item.description }}
+              />
+
+              {isExternal ? (
+                <a
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-gray-600 text-white text-sm px-6 py-2 rounded-xl hover:bg-gray-800 transition"
+                  aria-label={item.title}
+                >
+                  Ver &rsaquo;
+                </a>
+              ) : (
+                <Link
+                  to={item.url}
+                  className="bg-gray-600 text-white text-sm px-6 py-2 rounded-xl hover:bg-gray-800 transition"
+                >
+                  Ver &rsaquo;
+                </Link>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
-
 
 export default DirectAccessList;
