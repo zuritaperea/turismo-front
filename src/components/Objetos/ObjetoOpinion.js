fc2066550dd4d9349a189001689c4bebfb0cceed
Franco Zurita Perea <francozurita@gmail.com>
2025-06-30 13:31:42 -0300

    fix 2. Si no vamos a mostrar los comentarios por definicion de Mar Chiquita. En la seccion opiniones se debe sacar la leyenda Comentarios:0

--- src/components/Objetos/ObjetoOpinion.js
+++ src/components/Objetos/ObjetoOpinion.js
@@ -1,6 +1,8 @@
 import { useTranslation } from "react-i18next";
 import Estrellas from "../Items/Estrellas";
 import { User } from 'lucide-react';
+import { ConfigContext } from "antd/lib/config-provider";
+import { useEffect } from "react";
 
 const Comentario = ({ item }) => (
     <div key={item.id} className="border-b py-4">
@@ -17,15 +19,22 @@ const Comentario = ({ item }) => (
 
 const ObjetoOpinion = ({ objeto }) => {
     const { t } = useTranslation();
+    const config = useContext(ConfigContext);
+    const [mostrarComentarios, setMostrarComentarios] = useState(false);
+    useEffect(() => {
+        if (config) {
+            setMostrarComentarios(config.mostrar_comentarios);
+        }
+    }, [config]);
     return (
         <div className="mt-8 mb-20">
             <div className="text-2xl font-bold text-slate-900 tracking-tight dark:text-slate-200 my-4">
                 {t('common.opiniones')}</div>
             <div className="mb-4">
                 <Estrellas puntuacion={objeto.puntuacion} size={'lg'} />
-                <p className="text-gray-500 mt-5">
+                {mostrarComentarios && (<p className="text-gray-500 mt-5">
                     {t('common.comentarios')}: <span className="text-gray-700 font-bold">{objeto.evaluaciones?.length || 0}</span>
-                </p>
+                </p>)}
             </div>
             {objeto.evaluaciones?.map((item) => (
                 <Comentario key={item.id} item={item} />
