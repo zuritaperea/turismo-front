43780ffa9b628aa8aea15658bfb7bafd47e50628
Franco Zurita Perea <francozurita@gmail.com>
2025-07-16 12:05:44 -0300

    vuelvo a /web

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
       <AuthProvider>
         <>
           <CookieConsent onConsent={handleConsent} />
-          <BrowserRouter basename="/sitio">
+          <BrowserRouter basename="/web">
             <Routes>
               <Route path="/" element={<Inicio />} />
               <Route path="/login" element={<Login />} />
