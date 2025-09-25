46856e1c6d8038836799838782261be89749023c
Franco Zurita Perea <francozurita@gmail.com>
2025-09-25 11:58:47 -0300

    comercios screen

--- src/screens/Comercios.js
+++ src/screens/Comercios.js
@@ -1,12 +1,14 @@
 import objetoService from "../axios/services/comercio";
 import ObjetosScreen from "../components/Objetos/Objetos";
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export default function ComerciosScreen() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const target = "Comercio";
-  const title = "Balnearios";
+  const title = t(common.comercios);
 
   return (
     <ObjetosScreen target={target} navigation={navigate} objetoService={objetoService} title={title} />
