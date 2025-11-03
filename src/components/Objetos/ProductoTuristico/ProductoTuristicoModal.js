d1ee0b1d37b37866832af53f9b47ad936bd7a776
Franco Zurita Perea <francozurita@gmail.com>
2025-11-03 14:00:18 -0300

    se agrega filtro de maxima cantidad de personas

--- src/components/Objetos/ProductoTuristico/ProductoTuristicoModal.js
+++ src/components/Objetos/ProductoTuristico/ProductoTuristicoModal.js
@@ -71,13 +71,13 @@ const ProductoTuristicoModal = ({
                 {/* Columna izquierda: info del producto */}
                 <div className="">
                   <h1 className="text-gray-200 font-bold text-xl mb-4">{producto.attributes.name}</h1>
-                  
+
                   {producto.attributes.contenidos?.length > 0 && (
                     <div className="mb-4">
-                      <Carousel images={producto.attributes.contenidos}/>
+                      <Carousel images={producto.attributes.contenidos} />
                     </div>
                   )}
-                  
+
                   <div className="bg-gray-800 bg-opacity-50 rounded-lg p-4 mb-4">
                     <p className="text-gray-300 leading-relaxed text-sm">{producto.attributes.description}</p>
                   </div>
@@ -130,7 +130,7 @@ const ProductoTuristicoModal = ({
                             scroll={{ enabled: false }}
                             months={1}
                             className="rounded border shadow w-full"
-                            style={{ 
+                            style={{
                               position: 'relative',
                               zIndex: 10,
                               overflow: 'visible'
@@ -153,7 +153,7 @@ const ProductoTuristicoModal = ({
                             minDate={minDate}
                             maxDate={maxDate}
                             className="rounded border shadow w-full"
-                            style={{ 
+                            style={{
                               position: 'relative',
                               zIndex: 10,
                               overflow: 'visible'
@@ -173,108 +173,107 @@ const ProductoTuristicoModal = ({
                       <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-200 mb-1">
                           {t("reservas.horario_disponible")}
-                          </label>
-                          <select
-                            className="w-full border rounded p-2"
-                            onChange={(e) => {
-                              const selectedHour = e.target.value;
-                              if (selectedHour && intervaloMinutos) {
-                                const selectedStart = new Date(dateRange[0].startDate);
-                                const [hours, minutes] = selectedHour.split(":");
-                                selectedStart.setHours(hours, minutes, 0, 0);
-                                setSelectedStartDate(selectedStart);
+                        </label>
+                        <select
+                          className="w-full border rounded p-2"
+                          onChange={(e) => {
+                            const selectedHour = e.target.value;
+                            if (selectedHour && intervaloMinutos) {
+                              const selectedStart = new Date(dateRange[0].startDate);
+                              const [hours, minutes] = selectedHour.split(":");
+                              selectedStart.setHours(hours, minutes, 0, 0);
+                              setSelectedStartDate(selectedStart);
 
-                                const selectedEnd = new Date(selectedStart);
-                                selectedEnd.setMinutes(selectedEnd.getMinutes() + intervaloMinutos);
-                                setSelectedEndDate(selectedEnd);
-                              }
-                            }}
-                          >
-                            <option value="">{t("reservas.seleccionar_horario")}</option>
-                            {Object.entries(horariosDisponibles).map(([hora, disponible]) =>
-                              disponible > 0 ? (
-                                <option key={hora} value={hora}>
-                                  {hora}
-                                </option>
-                              ) : null
-                            )}
-                          </select>
-                        </div>
-                      )}
+                              const selectedEnd = new Date(selectedStart);
+                              selectedEnd.setMinutes(selectedEnd.getMinutes() + intervaloMinutos);
+                              setSelectedEndDate(selectedEnd);
+                            }
+                          }}
+                        >
+                          <option value="">{t("reservas.seleccionar_horario")}</option>
+                          {Object.entries(horariosDisponibles).map(([hora, disponible]) =>
+                            disponible > 0 ? (
+                              <option key={hora} value={hora}>
+                                {hora}
+                              </option>
+                            ) : null
+                          )}
+                        </select>
+                      </div>
+                    )}
 
-                      {tieneHorariosConfigurados && !hayHorariosParaElDia && (
-                        <p className="text-sm text-yellow-300 mt-2">
-                          {t("reservas.no_hay_horarios")}
-                        </p>)}
+                    {tieneHorariosConfigurados && !hayHorariosParaElDia && (
+                      <p className="text-sm text-yellow-300 mt-2">
+                        {t("reservas.no_hay_horarios")}
+                      </p>)}
 
+                    <div className="mb-4">
+                      <label className="block text-sm font-medium text-gray-200 mb-1">
+                        {t("reservas.cantidad_reservar")}
+                      </label>
+                      <input
+                        type="number"
+                        min={1}
+                        value={cantidadProducto}
+                        onChange={(e) => setCantidadProducto(Number(e.target.value))}
+                        className="w-full border rounded p-2"
+                      />
+                    </div>
+                    {!cantidadPersonasFuePasada && producto?.attributes?.maximum_number_persons > 1 && (
                       <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-200 mb-1">
-                          {t("reservas.cantidad_reservar")}
+                          {t("reservas.cantidad_personas")}
                         </label>
                         <input
                           type="number"
                           min={1}
-                          value={cantidadProducto}
-                          onChange={(e) => setCantidadProducto(Number(e.target.value))}
+                          value={cantidadPersonas}
+                          onChange={(e) => setCantidadPersonas(Number(e.target.value))}
                           className="w-full border rounded p-2"
                         />
                       </div>
-                      {!cantidadPersonasFuePasada && (
-                        <div className="mb-4">
-                          <label className="block text-sm font-medium text-gray-200 mb-1">
-                            {t("reservas.cantidad_personas")}
-                          </label>
-                          <input
-                            type="number"
-                            min={1}
-                            value={cantidadPersonas}
-                            onChange={(e) => setCantidadPersonas(Number(e.target.value))}
-                            className="w-full border rounded p-2"
-                          />
-                        </div>
-                      )}
+                    )}
 
-                      <FormularioAcompaniantes
-                        cantidadPersonas={cantidadPersonas}
-                        acompaniantes={acompaniantes}
-                        setAcompaniantes={setAcompaniantes}
-                      />
-                    </div>
+                    <FormularioAcompaniantes
+                      cantidadPersonas={cantidadPersonas}
+                      acompaniantes={acompaniantes}
+                      setAcompaniantes={setAcompaniantes}
+                    />
                   </div>
                 </div>
-              </>)}
-            </Modal.Body>
-            <Modal.Footer>
-              <div className="flex justify-center w-full">
-                <button
-                  className={`text-white rounded-xl py-4 px-8 flex items-center font-semibold text-lg transition-all duration-300 shadow-lg transform ${
-                    isLoadingReserva 
-                      ? "bg-gray-500 cursor-not-allowed" 
-                      : botonHabilitado 
-                        ? "bg-blue-600 hover:bg-blue-700 hover:scale-105 hover:shadow-xl cursor-pointer" 
-                        : "bg-gray-500 cursor-not-allowed opacity-50"
-                  }`}
-                  disabled={!botonHabilitado || isLoadingReserva}
-                  onClick={botonHabilitado ? handleReservar : undefined}
-                >
-                  {isLoadingReserva ? (
-                    <>
-                      <svg className="animate-spin h-5 w-5 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z" />
-                      </svg>
-                      {t("reservas.reservando")}
-                    </>
-                  ) : (
-                    <>
-                      <ArrowUpRight className="w-5 h-5 mr-2" />
-                      {t("reservas.reservar")}
-                    </>
-                  )}
-                </button>
               </div>
-            </Modal.Footer>
-          </div>
+            </>)}
+          </Modal.Body>
+          <Modal.Footer>
+            <div className="flex justify-center w-full">
+              <button
+                className={`text-white rounded-xl py-4 px-8 flex items-center font-semibold text-lg transition-all duration-300 shadow-lg transform ${isLoadingReserva
+                    ? "bg-gray-500 cursor-not-allowed"
+                    : botonHabilitado
+                      ? "bg-blue-600 hover:bg-blue-700 hover:scale-105 hover:shadow-xl cursor-pointer"
+                      : "bg-gray-500 cursor-not-allowed opacity-50"
+                  }`}
+                disabled={!botonHabilitado || isLoadingReserva}
+                onClick={botonHabilitado ? handleReservar : undefined}
+              >
+                {isLoadingReserva ? (
+                  <>
+                    <svg className="animate-spin h-5 w-5 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z" />
+                    </svg>
+                    {t("reservas.reservando")}
+                  </>
+                ) : (
+                  <>
+                    <ArrowUpRight className="w-5 h-5 mr-2" />
+                    {t("reservas.reservar")}
+                  </>
+                )}
+              </button>
+            </div>
+          </Modal.Footer>
+        </div>
       </Modal></div>
   );
 };
